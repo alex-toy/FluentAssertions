587193130c3bf704215560cd494c3fbb7a105bf3
alex-toy <alexei.80@hotmail.fr>
2023-02-26 08:32:59 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# FluentAssertions
+# Fluent Assertions
+
+## Install packages
+```
+XUnit
+FluentAssertions
+```
+
+## Run Unit Tests
+
+<img src="/pictures/unit_tests.png" title="unit tests"  width="800">
---
+++ TestProject/TestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace TestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var x = "value";
+
+            x.Should().Be("value");
+        }
+    }
+}
